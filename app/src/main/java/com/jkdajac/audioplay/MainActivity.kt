f4f9dae30870e7388788563c178f1348c5393090
ARTEM <jkda@ukr.net>
2021-08-01 13:02:00 +0300

    completed

--- app/src/main/java/com/jkdajac/audioplay/MainActivity.kt
+++ app/src/main/java/com/jkdajac/audioplay/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.jkdajac.audioplay
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btStart.setOnClickListener {
+            startService(Intent(this, AudioService::class.java))
+        }
+
+        btStop.setOnClickListener {
+            stopService(Intent(this, AudioService::class.java))
+        }
     }
 }
